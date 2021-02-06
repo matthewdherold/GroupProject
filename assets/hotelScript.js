c1685c8e0952982b9bcccaa5811d403787e2c7b0
Sarness Prasad <sunny776@yahoo.com>
2021-02-05 20:52:24 -0800

    got the data we need

--- assets/hotelScript.js
+++ assets/hotelScript.js
@@ -1,14 +1,66 @@
+var cityname ="new york"
+
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://hotels4.p.rapidapi.com/locations/search?query=new%20york&locale=en_US",
+	"url": "https://hotels4.p.rapidapi.com/locations/search?query="+cityname+"&locale=en_US",
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": "c50f8e3cedmshc2750e74357945fp1b0b2ajsn6ffdaf598689",
+		"x-rapidapi-key": "24e0114115msha91991ed1a11cd9p1e13b9jsn62259ea0d893",
+		"x-rapidapi-host": "hotels4.p.rapidapi.com"
+
+	}
+};
+
+
+$.ajax(settings).done(function (data) {
+	console.log(data);
+	console.log(data.suggestions[0].entities[0].destinationId);
+var destinationId =data.suggestions[0].entities[0].destinationId;
+
+console.log(destinationId);
+
+
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://hotels4.p.rapidapi.com/properties/list?destinationId="+ destinationId+ "&pageNumber=1&checkIn=2020-01-08&checkOut=2020-01-15&pageSize=25&adults1=1&currency=USD&locale=en_US&sortOrder=PRICE",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "24e0114115msha91991ed1a11cd9p1e13b9jsn62259ea0d893",
 		"x-rapidapi-host": "hotels4.p.rapidapi.com"
 	}
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+$.ajax(settings).done(function (data) {
+	console.log(data);
+	
+
+	for (let i = 0; i < 5; i++) {
+		
+		
+	
+	console.log(data.data.body.searchResults.results[i].name);
+		var addRess = data.data.body.searchResults.results[i].address
+		var streetAdd = addRess.streetAddress;
+		var Local = addRess.locality;
+		var regn = addRess.region;
+		var ZipCode = addRess.postalCode;
+
+	console.log(streetAdd + " " + Local + " " + regn + ", " + ZipCode);
+	console.log(data.data.body.searchResults.results[i].ratePlan.price.current);
+	console.log(data.data.body.searchResults.results[i].starRating);
+	console.log(data.data.body.searchResults.results[i].thumbnailUrl);
+}
+
 });
+
+
+
+
+
+
+
+});
+
