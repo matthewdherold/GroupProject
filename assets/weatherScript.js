ba487413b3c1b7d79ed4790b1716a40200c0c1d6
Matt H <matthewdherold@gmail.com>
2021-02-09 19:47:33 -0800

    finishing up

--- assets/weatherScript.js
+++ assets/weatherScript.js
@@ -2,17 +2,17 @@ var searchButton = $("#searchButton");
 var apiKey = "2ab902a0fe313d71d91734e79f858556";
 var keyCount = 0;
 
-searchButton.click(function () { 
+searchButton.click(function () {
     var forecastList = $("#forecastList")
-	var userInput = $("#searchBar").val();
-	var urlfiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&Appid=" + apiKey + "&units=imperial";
+    var userInput = $("#searchBar").val();
+    var urlfiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&Appid=" + apiKey + "&units=imperial";
 
 
-	if (userInput == "") {
-		console.log(userInput);
-	}else {
+    if (userInput == "") {
         console.log(userInput);
-		$.ajax({
+    } else {
+        console.log(userInput);
+        $.ajax({
             url: urlfiveDay,
             method: "GET"
         }).then(function (response) {
@@ -27,20 +27,20 @@ searchButton.click(function () {
                 var fiveDayForecast = new Date(response.list[i].dt * 1000);
                 fiveDayForecast = fiveDayForecast.toLocaleDateString("en-US");
 
-                
-    
+
+
                 forecastList.append(
-                    "<div class='fiveDayColor' style='background-color: rgb(195, 235, 241); padding: 2ch;'>" + 
-                    "<h2>" + fiveDayForecast + "</h2>" + 
-                    "<p>" + "Forecast: " + response.list[i].weather[0].description + "</p>" + 
-                    "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + 
-                    "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + 
+                    "<div class='fiveDayColor' style='padding: 2ch;'>" +
+                    "<h2>" + fiveDayForecast + "</h2>" +
+                    "<p>" + "Forecast: " + response.list[i].weather[0].description + "</p>" +
+                    "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" +
+                    "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" +
                     "</div>");
 
                 console.log(response.list);
             })
 
         });
-	}
+    }
 
 });
